be897035fecb9c1152b2166417eb9829d20adab3
Oliver-IV <145882378+Oliver-IV@users.noreply.github.com>
2024-08-27 18:23:47 -0700

    Boilerplate

---
+++ backend/src/main/java/dev/compaoli/finance/OliFinanceServerApplication.java
@@ -0,0 +1,13 @@
+package dev.compaoli.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OliFinanceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OliFinanceServerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/compaoli/finance/OliFinanceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.compaoli.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OliFinanceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
